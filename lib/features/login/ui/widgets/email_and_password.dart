1980f0127ab52108267ba1ae3c24558143b0ad25
hazzem salah <hazzemsalaht6@gmail.com>
2024-09-11 23:06:37 +0300

    - added do - added AppRegex - Integrated with login_EP - added login repo & cubit - added login states with validation

---
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -0,0 +1,102 @@
+import 'package:doctor_app/core/helpers/app_regex.dart';
+import 'package:doctor_app/core/helpers/spacing.dart';
+import 'package:doctor_app/core/widgets/app_text_form_field.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_cubit.dart';
+import 'package:doctor_app/features/login/ui/widgets/password_validation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class EmailAndPassword extends StatefulWidget {
+  const EmailAndPassword({super.key});
+
+  @override
+  State<EmailAndPassword> createState() => _EmailAndPasswordState();
+}
+
+class _EmailAndPasswordState extends State<EmailAndPassword> {
+  bool isObscureText = true;
+
+  bool hasLowerCase = false;
+  bool hasUpperCase = false;
+  bool hasSpecialCharacters = false;
+  bool hasNumber = false;
+  bool hasMinLength = false;
+
+  late TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordController = context.read<LoginCubit>().passwordController;
+    setupPasswordControllerListener();
+  }
+
+  void setupPasswordControllerListener() {
+    passwordController.addListener(() {
+      setState(() {
+        hasLowerCase = AppRegex.hasLowerCase(passwordController.text);
+        hasUpperCase = AppRegex.hasUpperCase(passwordController.text);
+        hasSpecialCharacters =
+            AppRegex.hasSpecialCharacter(passwordController.text);
+        hasNumber = AppRegex.hasNumber(passwordController.text);
+        hasMinLength = AppRegex.hasMinLength(passwordController.text);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: context.read<LoginCubit>().formKey,
+      child: Column(
+        children: [
+          AppTextFormField(
+            hintText: "Email",
+            validator: (email) {
+              if (email == null ||
+                  email.isEmpty ||
+                  !AppRegex.isEmailValid(email)) {
+                return 'Please enter a vaild Email';
+              }
+            },
+            controller: context.read<LoginCubit>().emailController,
+          ),
+          verticalSpace(18),
+          AppTextFormField(
+            controller: context.read<LoginCubit>().passwordController,
+            hintText: "password",
+            isObscureText: isObscureText,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isObscureText = !isObscureText;
+                });
+              },
+              child: Icon(
+                isObscureText ? Icons.visibility_off : Icons.visibility,
+              ),
+            ),
+            validator: (password) {
+              if (password == null || password.isEmpty) {
+                return 'Please enter a vaild Password';
+              }
+            },
+          ),
+          verticalSpace(24),
+          PasswordValidations(
+            hasLowerCase: hasLowerCase,
+            hasUpperCase: hasUpperCase,
+            hasSpecialCharacters: hasSpecialCharacters,
+            hasNumber: hasNumber,
+            hasMinLength: hasMinLength,
+          ),
+        ],
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+}
